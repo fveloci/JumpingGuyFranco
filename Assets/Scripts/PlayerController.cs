e01b8e077925cbfb5d880ef0ed86c87da51666b8
Franco Veloci <fveloci@hotmail.com>
2020-11-05 19:07:01 -0300

    Juego hasta video 15

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject game;
+    public GameObject enemyGenerator;
+
     private Animator animator;
 
     // Start is called before the first frame update
@@ -15,12 +18,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool gamePlaying = game.GetComponent<GameController>().gameState == GameState.Playing;
+        if(gamePlaying &&(Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))){
+            UpdateState("PlayerJump");
+        }
     }
 
     public void UpdateState(string state = null){
         if( state != null){
             animator.Play(state);
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "Enemy"){
+            UpdateState("PlayerDie");
+            game.GetComponent<GameController>().gameState = GameState.Ended;
+            enemyGenerator.SendMessage("CancelGenerator", true);
+            
+            game.GetComponent<AudioSource>().Stop();
+        }
+        
+    }
+
+    void GameReady(){
+        game.GetComponent<GameController>().gameState = GameState.Ready;
     }
 }
